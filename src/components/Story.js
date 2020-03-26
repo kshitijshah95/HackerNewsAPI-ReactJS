8d653a5a9293a11769475941f26aa6b6ffe9d288
Kshitij Shah <kshitijshah95@gmail.com>
2020-03-26 13:23:59 -0700

    HacklerNewsAPIv1.0

---
+++ src/components/Story.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+import { getStory } from "../services/hnAPI";
+import {
+  StoryWrapper,
+  StoryTitle,
+  StoryMeta,
+  StoryMetaElement
+} from "../styles/StoryStyles";
+import { mapTime } from '../mappers/mapTime'
+
+export const Story = ({ storyId }) => {
+  const [story, setStory] = useState({});
+
+  useEffect(() => {
+    getStory(storyId).then(data => {
+      console.log(data);
+      return data && data.url && setStory(data);
+    });
+  }, []);
+
+  return story && story.url ? (
+    <StoryWrapper>
+      <StoryTitle>
+        <a href={story.url}>{story.title}</a>
+      </StoryTitle>
+
+      <StoryMeta>
+        <span data-testid="story-by">
+          <StoryMetaElement color="#000">By:</StoryMetaElement> {story.by}
+        </span>
+        <span data-testid="story-time">
+          <StoryMetaElement color="#000">Posted:</StoryMetaElement> {` `}
+          {mapTime(story.time)}
+        </span>
+      </StoryMeta>
+    </StoryWrapper>
+  ) : null;
+};
