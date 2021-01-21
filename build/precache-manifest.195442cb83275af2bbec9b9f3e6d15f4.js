b1afe9efdd549d77acf4b8a26ec209b38b266fd7
Kshitij Shah <kshitijshah95@gmail.com>
2021-01-20 19:12:21 -0500

    Build

---
+++ build/precache-manifest.195442cb83275af2bbec9b9f3e6d15f4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4463ea4e080df344390b552d220a851",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0b3523157d32ddd8741",
+    "url": "/static/js/2.69ecd2c0.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/static/js/2.69ecd2c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34037b0e8839375fd889",
+    "url": "/static/js/main.7a1d6dda.chunk.js"
+  },
+  {
+    "revision": "9eed816694825d220005",
+    "url": "/static/js/runtime-main.9fd3efb7.js"
+  }
+]);
